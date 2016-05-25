9351f7b51136e754b502721b4365180afc563f4e
amfo <antonio.fernandez@gmx.com>
2016-05-25 15:12:47 +0200

    Aclarando las licencias

--- include/gfx.h
+++ include/gfx.h
@@ -1,22 +1,22 @@
-/* Copyright (c) 2015-2016, Antonio M. Fernández Orts <antonio.fernandez at gmx dot com>
+/* Copyright (C) 2002 W.P. van Paassen - peter@paassen.tmfweb.nl
  *
- * This file is part of RockPush.
- *
- * RockPush is free software: you can redistribute it and/or modify *
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * RockPush is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with RockPush.  If not, see <http://www.gnu.org/licenses/>.
- *
- *
- */
+ T his program is free software; you can redistribute it and/or modify it under                                    *
+ the terms of the GNU General Public License as published by the Free
+ Software Foundation; either version 2 of the License, or (at your
+ option) any later version.
+
+ This program is distributed in the hope that it will be useful, but WITHOUT
+ ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ for more details.
+
+     You should have received a copy of the GNU General Public License
+     along with this program; see the file COPYING.  If not, write to the Free
+     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.  */
+
+/* note that the code has not been optimized*/
+/* for instance instead of clearing and updating the complete scroll area */
+/* it would be nicer to remember each letter its coordinates and clearing these and updating the new ones instead */
 
 #include <stdio.h>
 #include <stdlib.h>
